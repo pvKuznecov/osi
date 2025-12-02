b29abd2d561042fd5148529e14146a84d5ef3d8e
Kuznetsov Pavel <p.v.kuznecov@yandex.ru>
2025-12-02 16:48:55 +0700

    *

--- README.md
+++ README.md
@@ -4,4 +4,11 @@
 <h3>Полезно:</h3>
 <ul>
     <li>Убить процессы на порту:<br><code>npx kill-port *порт*</code></li>
+    <li>
+        Пример доработки <i>vue.config.js</i> - сборка проекта в папку для дальнейшего размещения его на gitlab webpages:<br>
+        <code>
+        publicPath: process.env.NODE_ENV === "production" ? "/osi/" : "/",<br>
+        outputDir: "docs",
+        </code>
+    </li>
 </ul>
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/img/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+<style src="./global.css"></style>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -13,14 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
