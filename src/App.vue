cc62a2f5110831422e2454ed12e332e7ebea0f00
Kuznetsov Pavel <p.v.kuznecov@yandex.ru>
2025-12-03 16:55:57 +0700

    *

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/img/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="webos">
+    <DesktopArea>
+      <SimpleWindow
+        v-for="window in osStore.windows"
+        :key="window.id"
+        :windowId="window.id"
+        :title="window.title"
+        :appName="window.appName"
+        @close="closeWindow"
+        @focus="focusWindow"
+      />
+    </DesktopArea>
+    <TaskBar />
+  </div>
 </template>
-<style src="./global.css"></style>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DesktopArea from './components/os/DesktopArea/DesktopArea.vue'
+import TaskBar from './components/os/TaskBar.vue'
+import SimpleWindow from './components/os/SimpleWindow/SimpleWindow.vue'
+import { useOsStore } from './stores/os.store'
 
 export default {
   name: 'App',
+  
   components: {
-    HelloWorld
+    DesktopArea,
+    TaskBar,
+    SimpleWindow
+  },
+  
+  data() {
+    return {
+      osStore: null
+    }
+  },
+  
+  methods: {
+    closeWindow(windowId) {
+      if (this.osStore) {
+        this.osStore.closeWindow(windowId)
+      }
+    },
+    
+    focusWindow(windowId) {
+      if (this.osStore) {
+        this.osStore.activeWindowId = windowId
+      }
+    }
+  },
+  
+  created() {
+    // Инициализируем хранилище
+    this.osStore = useOsStore()
+  },
+  
+  mounted() {
+    // Открываем демо-окно при запуске
+    setTimeout(() => {
+      if (this.osStore) {
+        this.osStore.openWindow('notepad')
+      }
+    }, 500)
   }
 }
 </script>
+
+<style scoped>
+.webos {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+</style>
